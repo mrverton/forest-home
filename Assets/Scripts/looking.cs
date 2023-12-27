f4573a7356a68148675e04aab5f1190908f64379
mrverton <mrverton@gmail.com>
2023-12-27 19:52:31 +0300

    1.3
    new scirpts and fixex bugs a lot!

--- Assets/Scripts/looking.cs
+++ Assets/Scripts/looking.cs
@@ -1,23 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class looking : MonoBehaviour
 {
     Ray ray;
+    [SerializeField] GameObject player, chest, inv;
     public LayerMask layer;
+    bool openInv = false;
     private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (openInv)
+            {
+                openInv = false;
+                player.GetComponent<PlayerInput>().enabled = true;
+                inv.SetActive(false);
+                chest.SetActive(false);
+            }
+            else
+            {
+                openInv = true;
+                player.GetComponent<PlayerInput>().enabled = false;
+                inv.SetActive(true);
+            }
+        }
         ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * 10);
 
         
         RaycastHit raycast;
+
+
+
         if(Physics.Raycast(ray,out raycast,10,layer))
-        {
-            Debug.Log(raycast.transform.tag);
+        {   
+           if(raycast.transform.tag=="Chest")
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+
+
+                    chest.SetActive(true);
+                    inv.SetActive(true);
+                    player.GetComponent<PlayerInput>().enabled = false;
+                    openInv = true;
+                }
+            }
         }
+       
     }
 
 }
--- Assets/Scripts/temp/treePlay.cs
+++ Assets/Scripts/temp/treePlay.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tree : MonoBehaviour
+public class treePlay : MonoBehaviour
 {
+    [SerializeField] GameObject tree;
+    [SerializeField] GameObject parent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        parent = GameObject.Find("parent");
+        var Obj = Instantiate(tree, transform);
+        Obj.transform.parent = parent.transform;
+
     }
 
     // Update is called once per frame
